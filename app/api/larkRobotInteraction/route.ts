306cb25d86249cbaba67ba6f46232d5044038e79
pg-beau <pg.beau@outlook.com>
2023-07-27 16:56:08 +0800

    js => ts

--- app/api/larkRobotInteraction/route.ts
+++ app/api/larkRobotInteraction/route.ts
@@ -2,8 +2,8 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-07-20 14:43:45
  * @LastEditors: pg-beau pg.beau@outlook.com
- * @LastEditTime: 2023-07-27 14:45:20
- * @FilePath: /WorkSpace/trading-straregy/app/api/larkRobotInteraction/route.js
+ * @LastEditTime: 2023-07-27 16:53:58
+ * @FilePath: /WorkSpace/trading-straregy/app/api/larkRobotInteraction/route.ts
  * @Description:
  *
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
@@ -20,7 +20,20 @@ import { NextResponse } from 'next/server';
 //   await POST();
 // });
 
-export async function POST(request) {
+export async function POST() {
+  interface BinanceFundingRateData {
+    symbol: string;
+    lastFundingRate: string;
+  }
+
+  interface FilterDataItem {
+    symbol: string;
+    lastFundingRate: string;
+    openInterestStatistics: string;
+    contractPositionGrowth: string;
+    timestamp: string;
+  }
+
   const getBinanceFundingRateData = async () => {
     try {
       const res = await fetch(`https://fapi.binance.com/fapi/v1/premiumIndex`);
@@ -32,7 +45,7 @@ export async function POST(request) {
     }
   };
 
-  const getBinanceOpenInterestStatistics = async (symbol) => {
+  const getBinanceOpenInterestStatistics = async (symbol: string) => {
     try {
       const res = await fetch(
         `https://fapi.binance.com/futures/data/openInterestHist?symbol=${symbol}&period=5m&limit=288`
@@ -45,7 +58,7 @@ export async function POST(request) {
     }
   };
 
-  const convertTZ = (timestamp, tzString) => {
+  const convertTZ = (timestamp: string, tzString: string) => {
     // 将timestamp转换成Date对象，并乘以1000，因为JavaScript的时间戳是毫秒为单位
     const date = new Date(timestamp);
     // 使用toLocaleString方法，指定时区为tzString，并返回结果
@@ -53,7 +66,7 @@ export async function POST(request) {
   };
 
   //获取资金费率所有数据
-  const binanceFundingRateData = await getBinanceFundingRateData();
+  const binanceFundingRateData: BinanceFundingRateData[] = await getBinanceFundingRateData();
 
   const filterFundingRateData = binanceFundingRateData.filter(
     ({ lastFundingRate }) => Math.abs(Number(lastFundingRate)) >= 0.001
@@ -65,25 +78,26 @@ export async function POST(request) {
   }
 
   // 筛选出同时符合资金费率和合约持仓增长量的币种
+
   const filterData = (
     await Promise.all(
       filterFundingRateData.map(async ({ symbol, lastFundingRate }) => {
         const openInterestStatisticsData = await getBinanceOpenInterestStatistics(symbol);
 
-        const firstSumOpenInterestValue = Number(openInterestStatisticsData[0].sumOpenInterestValue);
-        const lastSumOpenInterestValue = Number(
-          openInterestStatisticsData[openInterestStatisticsData.length - 1].sumOpenInterestValue
-        );
+        const firstSumOpenInterestValue = openInterestStatisticsData[0];
+        const lastSumOpenInterestValue = openInterestStatisticsData[openInterestStatisticsData.length - 1];
 
         const contractPositionGrowth =
-          (lastSumOpenInterestValue - firstSumOpenInterestValue) / firstSumOpenInterestValue;
+          (Number(lastSumOpenInterestValue.sumOpenInterestValue) -
+            Number(firstSumOpenInterestValue.sumOpenInterestValue)) /
+          Number(firstSumOpenInterestValue.sumOpenInterestValue);
 
         const contractPositionGrowthPercentage = (contractPositionGrowth * 100).toFixed(2) + '%';
 
         if (contractPositionGrowth >= 0.4) {
           return {
             symbol,
-            lastFundingRate: (lastFundingRate * 100).toFixed(4) + '%',
+            lastFundingRate: (Number(lastFundingRate) * 100).toFixed(4) + '%',
             openInterestStatistics: Number(
               openInterestStatisticsData[openInterestStatisticsData.length - 1].sumOpenInterestValue
             ).toFixed(2),
@@ -102,17 +116,17 @@ export async function POST(request) {
     return NextResponse.json({ msg: `No Tokens Meet The 24h Contract Position Growth Conditions` });
   }
 
-  const strFilterData = filterData.map(
-    ({ symbol, lastFundingRate, contractPositionGrowth, openInterestStatistics, timestamp }) => {
+  const strFilterData = filterData.map((item) => {
+    if (item) {
       return `
-        代币名称: ${symbol};
-        资金费率: ${lastFundingRate};
-        24H合约增长量: ${contractPositionGrowth};
-        合约持仓市值: ${openInterestStatistics};
-        更新时间: ${timestamp}
+        代币名称: ${item.symbol};
+        资金费率: ${item.lastFundingRate};
+        24H合约增长量: ${item.contractPositionGrowth};
+        合约持仓市值: ${item.openInterestStatistics};
+        更新时间: ${item.timestamp}
         `;
     }
-  );
+  });
 
   const LarkData = {
     msg_type: 'text',
@@ -122,7 +136,7 @@ export async function POST(request) {
     },
   };
 
-  const postLarkRes = await fetch(process.env.LARK_HOOK_URL, {
+  const postLarkRes = await fetch(process.env.LARK_HOOK_URL as string, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
