1111d41e1bc451d32976319143c2b28761c53411
pg-beau <pg.beau@outlook.com>
2023-07-28 14:48:24 +0800

    bug fix

--- app/api/larkRobotInteraction/route.ts
+++ app/api/larkRobotInteraction/route.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-07-20 14:43:45
  * @LastEditors: pg-beau pg.beau@outlook.com
- * @LastEditTime: 2023-07-28 14:45:09
+ * @LastEditTime: 2023-07-28 14:48:07
  * @FilePath: /WorkSpace/trading-straregy/app/api/larkRobotInteraction/route.ts
  * @Description:
  *
@@ -19,6 +19,8 @@ interface BinanceFundingRateData {
 
 export async function POST(request: Request) {
   const key = (request.body as { key: string } | null)?.key;
+  console.log(`key`);
+  console.log(key);
 
   if (key !== process.env.BEARER_TOKEN) {
     return NextResponse.json({ msg: `Invalid Token` }, { status: 401 });
