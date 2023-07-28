c0b2feff914a4a7517de6f97a469207a60a392f0
pg-beau <pg.beau@outlook.com>
2023-07-28 11:15:44 +0800

    add bearertoken

--- app/api/larkRobotInteraction/route.ts
+++ app/api/larkRobotInteraction/route.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-07-20 14:43:45
  * @LastEditors: pg-beau pg.beau@outlook.com
- * @LastEditTime: 2023-07-27 23:57:47
+ * @LastEditTime: 2023-07-28 11:14:31
  * @FilePath: /WorkSpace/trading-straregy/app/api/larkRobotInteraction/route.ts
  * @Description:
  *
@@ -16,8 +16,20 @@ interface BinanceFundingRateData {
   symbol: string;
   lastFundingRate: string;
 }
+interface Request {
+  headers: {
+    authorization: string;
+  };
+}
 
-export async function POST() {
+export async function POST(resquest: Request) {
+  const authHeader = resquest.headers.authorization;
+  const bearerToken = authHeader.split(' ')[1];
+
+  if (bearerToken !== process.env.BEARER_TOKEN) {
+    return NextResponse.json({ msg: `Invalid Token` }, { status: 401 });
+  }
+
   const getBinanceFundingRateData = async () => {
     try {
       const res = await fetch(`https://fapi.binance.com/fapi/v1/premiumIndex`);
