be34c56eb8cad9818aed45680e69a6ffaa9b1f7d
pg-beau <pg.beau@outlook.com>
2023-07-28 18:41:44 +0800

    rm request

--- app/api/larkRobotInteraction/route.ts
+++ app/api/larkRobotInteraction/route.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-07-20 14:43:45
  * @LastEditors: pg-beau pg.beau@outlook.com
- * @LastEditTime: 2023-07-28 18:30:31
+ * @LastEditTime: 2023-07-28 18:40:06
  * @FilePath: /WorkSpace/trading-straregy/app/api/larkRobotInteraction/route.ts
  * @Description:
  *
@@ -17,7 +17,7 @@ interface BinanceFundingRateData {
   lastFundingRate: string;
 }
 
-export async function GET(request: Request) {
+export async function GET() {
   const getBinanceFundingRateData = async () => {
     try {
       const res = await fetch(`https://fapi.binance.com/fapi/v1/premiumIndex`);
@@ -105,15 +105,8 @@ export async function GET(request: Request) {
   }
 }
 
-export async function POST(request: Request) {
-  // const jsonRequest = await request.json();
-  // const jsonKey = jsonRequest.body.key;
-
-  // if (jsonKey !== process.env.BEARER_TOKEN) {
-  //   return NextResponse.json({ msg: `Invalid Token`, request: jsonRequest, body: jsonRequest.body }, { status: 401 });
-  // }
-
-  const getFilterResponse = await GET(request);
+export async function POST() {
+  const getFilterResponse = await GET();
   const getFilterData = await getFilterResponse.json();
 
   if (Array.isArray(getFilterData) && getFilterData.length > 0) {
