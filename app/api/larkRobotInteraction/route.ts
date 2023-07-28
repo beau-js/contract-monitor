77d87f1ad2f5094993c2c82cb0622b1351d0f556
pg-beau <pg.beau@outlook.com>
2023-07-28 15:57:52 +0800

    add page.tsx

--- app/api/larkRobotInteraction/route.ts
+++ app/api/larkRobotInteraction/route.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-07-20 14:43:45
  * @LastEditors: pg-beau pg.beau@outlook.com
- * @LastEditTime: 2023-07-28 15:33:43
+ * @LastEditTime: 2023-07-28 15:57:19
  * @FilePath: /WorkSpace/trading-straregy/app/api/larkRobotInteraction/route.ts
  * @Description:
  *
---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+/*
+ * @Author: pg-beau pg.beau@outlook.com
+ * @Date: 2023-07-28 15:43:04
+ * @LastEditors: pg-beau pg.beau@outlook.com
+ * @LastEditTime: 2023-07-28 15:55:10
+ * @FilePath: /WorkSpace/trading-straregy/app/page.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+import { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [data, setData] = useState('No Data');
+
+  useEffect(() => {
+    fetch(`api/larkRobotInteraction`, {
+      method: `POST`,
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return <div>{data}</div>;
+};
+
+export default Home;
